c0621e0e8326699f810e7b22340b415ec530ff50
manuelfpugr <manuelfuenpl@correo.ugr.es>
2025-02-20 13:29:59 +0100

    changes

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://openfullcoursep2.onrender.com',
+        changeOrigin: true,
+      },
+    }
+  },
 })
